9d4f2ac6075153c5e977bb92e6fa0eab42533b7f
Hyuns008 <gustn4984@naver.com>
2024-11-03 18:48:04 +0900

    리스트,디스텐스,메니저,싱글턴

---
+++ Assets/Scripts/Scene/Scenemgr.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Scenemgr : MonoBehaviour
+{
+    private void Awake()
+    {
+        Shared.Scenemgr = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
