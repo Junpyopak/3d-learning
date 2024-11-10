15271ce3e3b43f3c79f554f1adf948e65c84d812
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-11-10 23:01:48 +0900

    로그인 ui고치기

--- Assets/Resorces/Scripts/UI/ui_tittle/uI_Login.cs
+++ Assets/Resorces/Scripts/UI/ui_tittle/uI_Login.cs
@@ -11,23 +11,25 @@ public class uI_Login : MonoBehaviour
     void Start()
     {
         //여기서 데이터 불러와서 계정이 있으면 로딩창(로비창)으로
-        
+        Shared.Scenemgr.GetPlayerPrefsStringKey("id");
+
+        Debug.Log(PlayerPrefs.GetString("id"));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnBtnLogin()
     {
-        
         Shared.Scenemgr.SetPlayerPrefsStringKey("id", textInput.text);
-        if(PlayerPrefs.GetString("id")==textInput.text)
-        {
-           Debug.Log(textInput.text);
-           Shared.Scenemgr.ChangeScene(eSCENE.LOBBY);
-        }
+        //if (Shared.Scenemgr.GetPlayerPrefsStringKey("id") == textInput.text)
+        //{
+        //    //Debug.Log(PlayerPrefs.GetString("id"));
+        //    //
+        //}
     }
 }
