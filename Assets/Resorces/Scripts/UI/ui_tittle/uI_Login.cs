0383ac056136a36558173fbdc7771d427486377e
Hyuns008 <gustn4984@naver.com>
2024-11-10 17:51:18 +0900

    로그인 시스템 중 값을불러와 없으면 타이틀로 있으면 로딩창으로

---
+++ Assets/Resorces/Scripts/UI/ui_tittle/uI_Login.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uI_Login : MonoBehaviour
+{
+    [SerializeField] Text textInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //여기서 데이터 불러와서 계정이 있으면 로딩창(로비창)으로
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBtnLogin()
+    {
+        if(textInput.text.Length<0 == true)
+        {
+            Shared.Scenemgr.ChangeScene(eSCENE.TITLE);
+        }
+    }
+}
