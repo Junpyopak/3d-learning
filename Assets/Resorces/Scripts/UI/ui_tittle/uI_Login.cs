d41113eb8b06695b45f5fa1751a09fd2f4463842
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-11-12 23:38:03 +0900

    로그인 ui (버튼을 한번더 누르면 아이디가 맞는지 확인하고 로그인)

--- Assets/Resorces/Scripts/UI/ui_tittle/uI_Login.cs
+++ Assets/Resorces/Scripts/UI/ui_tittle/uI_Login.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR;
@@ -7,29 +8,45 @@ using UnityEngine.XR;
 public class uI_Login : MonoBehaviour
 {
     [SerializeField] Text textInput;
+    [SerializeField] InputField inputField;
     // Start is called before the first frame update
     void Start()
     {
         //여기서 데이터 불러와서 계정이 있으면 로딩창(로비창)으로
-        Shared.Scenemgr.GetPlayerPrefsStringKey("id");
+        //Shared.Scenemgr.GetPlayerPrefsStringKey("id");
 
-        Debug.Log(PlayerPrefs.GetString("id"));
+        //if (Shared.Scenemgr.GetPlayerPrefsStringKey("id") != null)
+        //{
+        //    Debug.Log(PlayerPrefs.GetString("id"));
+        //    Shared.Scenemgr.ChangeScene(eSCENE.LOBBY);
+        //}
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+   
     }
 
     public void OnBtnLogin()
     {
-        Shared.Scenemgr.SetPlayerPrefsStringKey("id", textInput.text);
-        //if (Shared.Scenemgr.GetPlayerPrefsStringKey("id") == textInput.text)
-        //{
-        //    //Debug.Log(PlayerPrefs.GetString("id"));
-        //    //
-        //}
+
+        if (inputField.text == Shared.Scenemgr.GetPlayerPrefsStringKey("id"))
+        {
+            Shared.Scenemgr.ChangeScene(eSCENE.LOBBY);
+        }
+        else
+        {
+            Debug.Log("로그인 오류 ! 아이디를 확인하세요");
+        }
+    }
+
+    public void BtnJoin()
+    {
+        if (inputField.text.Length >= 2 || inputField.text.Length <= 7)
+        {
+            Shared.Scenemgr.SetPlayerPrefsStringKey("id", inputField.text);
+        }
     }
 }
